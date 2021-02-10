a4a3a424866c71fd4bc2d8f1aa6fbaa4f8e8fa58
Ketanchandel <77281254+Ketanchandel@users.noreply.github.com>
2021-02-10 18:18:25 +0530

    Add files via upload

---
+++ config/constants.php
@@ -0,0 +1,9 @@
+<?php 
+    //define('SITEURL','http://localhost:81/quizapp/');
+    define('LOCALHOST','localhost');
+    define('USERNAME','root');
+    define('PASSWORD','');
+    define('DBNAME','quizapp');
+
+    define('SITEURL', 'http://localhost/name_of_project_folder/'); //Update the home URL of the project if you have changed port number or it's live on server
+?>
